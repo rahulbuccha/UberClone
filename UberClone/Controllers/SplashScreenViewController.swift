e0f116ce4119c00d61ba8296b6a8cd8a93a5b78d
Rahul Shaw <rahulbucha69@gmail.com>
2022-07-20 11:13:43 +0530

    nice

--- UberClone/Controllers/SplashScreenViewController.swift
+++ UberClone/Controllers/SplashScreenViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SplashScreenViewController.swift
 //  UberClone
 //
 //  Created by Admin on 20/07/22.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+       
     }
 
-
 }
-
