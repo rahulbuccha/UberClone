03a2b0f6976973743a0ddec95ddb6d96e33c8a52
Rahul Shaw <rahulbucha69@gmail.com>
2022-07-22 17:57:22 +0530

    Launch splash screen animation

--- UberClone/Controllers/ActivityScreen/ActivityViewController.swift
+++ UberClone/Controllers/ActivityScreen/ActivityViewController.swift
@@ -20,8 +20,6 @@ class ActivityViewController: UIViewController {
         activityTableView.separatorStyle = .none
         
     }
-    
-
 
 }
 
--- UberClone/Controllers/PreViewController.swift
+++ UberClone/Controllers/PreViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import RevealingSplashView
 
 class PreViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        //Initialize a revealing Splash with with the iconImage, the initial size and the background color
+        let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "uberLogo")!,iconInitialSize: CGSize(width: 200, height: 200), backgroundColor: UIColor(red:0.0, green:0.0, blue:0.0, alpha:1.0))
+
+        //Adds the revealing splash view as a sub view
+        self.view.addSubview(revealingSplashView)
+        
+        revealingSplashView.animationType = SplashAnimationType.squeezeAndZoomOut
+        
+        
+        //Starts animation
+        revealingSplashView.startAnimation(){
+            print("Completed")
+        }
        
     }
 
